0cdec841ce5a4d11a92ea1563e5e3d6c5a108c3d
nik1998 <beregnovnikita@gmail.com>
2023-12-08 17:06:13 +0300

    correct adminka and check result

--- cs_backend/core/cstests/urls.py
+++ cs_backend/core/cstests/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from cstests.views import GetAllISTestAPIView, ISTestDetailView
+from cstests.views import GetAllISTestAPIView, ISTestDetailView, CheckResultView
 
 urlpatterns = [
     path('detail/<int:id>/', ISTestDetailView.as_view(), name='istest_detail'),
-    path("tests/", GetAllISTestAPIView.as_view())
+    path("tests/", GetAllISTestAPIView.as_view()),
+    path("check_result/", CheckResultView.as_view())
 ]
