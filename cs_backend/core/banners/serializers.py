ff5dda77a8a3f1d88142d08068d5394fafd3e192
nik1998 <beregnovnikita@gmail.com>
2023-12-09 02:00:36 +0300

    add search

--- cs_backend/core/banners/serializers.py
+++ cs_backend/core/banners/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from banners.models import SourceModel
 from pictures.serializers import PictureSerializer
+from utils.constants import FOR_AGES
 
 
 class BannerSerializer(serializers.Serializer):
@@ -11,6 +12,7 @@ class BannerSerializer(serializers.Serializer):
     link = serializers.URLField()
     type = serializers.CharField()
     images = PictureSerializer(many=True)
+    for_age = serializers.ChoiceField(choices=FOR_AGES)
 
 
 class SourceModelSerializer(serializers.ModelSerializer):
