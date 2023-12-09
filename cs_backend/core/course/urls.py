1e13678317ec8d62f830ba375b26903c4117f906
iushishko.adm <iushishko@sitronics.com>
2023-12-09 10:06:55 +0300

    view for course

--- cs_backend/core/course/urls.py
+++ cs_backend/core/course/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
+from course.views import CourseViewSet
 
+router = DefaultRouter()
+router.register(r"", CourseViewSet, basename="hotel")
 
 urlpatterns = [
+    path("", include(router.urls)),
     # path('detail/<int:id>/', ISTestDetailView.as_view(), name='istest_detail'),
     # path("tests/", GetAllISTestAPIView.as_view()),
     # path("check_result/", CheckResultView.as_view())
--- cs_backend/core/course/views.py
+++ cs_backend/core/course/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from course.models import Course
+from course.serializers import CourseSerializer
+from utils.pagination import ApiPagination
+
 
 # Create your views here.
+
+class CourseViewSet(viewsets.ModelViewSet):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all().order_by("-id")
+    pagination_class = ApiPagination
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    authentication_classes = [JWTAuthentication]
